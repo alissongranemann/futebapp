acb1909d1805a4404ea2b2cfc447ea20ec831f64
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2018-09-22 15:07:21 -0300

    add group with firebase

--- src/main/js/actions/groups.js
+++ src/main/js/actions/groups.js
@@ -1,65 +1,72 @@
 import uuid from 'uuid';
+import database from 'firebase/firebase'
 
-export const addGroup = (
-  {
-    id = '',
-    name = '',
-    players = [],
-    games = []
-  } = {}
-) => ({
-  type: 'ADD_GROUP',
-  group: {
-    id: id ? id : uuid(),
-    name,
-    players, 
-    games
-  }
+export const addGroup = (group) => ({
+    type: 'ADD_GROUP',
+    group
 });
 
+export const startAddGroup = (groupData = {}) => {
+    return (dispatch) => {
+        const {
+            name = '',
+            players = [],
+            games = []
+        } = groupData;
+        const group = { name, players, games };
+
+        database.ref('groups').push(group).then((ref) => {
+            dispatch(addGroup({
+                id: ref.key,
+                ...group
+            }));
+        });
+    };
+};
+
 export const removeGroup = ({ id } = {}) => ({
-  type: 'REMOVE_GROUP',
-  id
+    type: 'REMOVE_GROUP',
+    id
 });
 
 export const editGroup = (id, updates) => ({
-  type: 'EDIT_GROUP',
-  id,
-  updates
+    type: 'EDIT_GROUP',
+    id,
+    updates
 });
 
 export const addGame = (
-  groupId = '', 
-  {
-    id = '',
-    location = '',
-    numPlayers = -1,
-    date = '',
-    schedule = ''
-  }
+    groupId = '',
+    {
+        id = '',
+        location = '',
+        numPlayers = -1,
+        date = '',
+        schedule = ''
+    }
 ) => ({
-  type: 'ADD_GAME',
-  id,
-  game: {
-    id: id ? id : uuid(),
-    location,
-    numPlayers,
-    date,
-    schedule
-  }
+    type: 'ADD_GAME',
+    id,
+    game: {
+        id: id ? id : uuid(),
+        location,
+        numPlayers,
+        date,
+        schedule
+    }
 });
 
 export const addPlayer = (
-  groupId = '',
-  { 
-    id = '',
-    name = ''
-  }
+    groupId = '',
+    {
+        id = '',
+        name = ''
+    }
 ) => ({
-  type: 'ADD_PLAYER',
-  groupId,
-  player: {
-    id: id ? id : uuid(),
-    name
-  }
+    type: 'ADD_PLAYER',
+    groupId,
+    player: {
+        id: id ? id : uuid(),
+        name
+    }
 });
--- src/main/js/components/group/AddGroupPage.js
+++ src/main/js/components/group/AddGroupPage.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import GroupForm from './GroupForm';
-import { addGroup } from 'actions/groups';
+import { startAddGroup } from 'actions/groups';
 
 const AddGroupPage = (props) => (
   <div>
     <h1>Criar grupo</h1>
     <GroupForm
       onSubmit={(group) => {
-        props.dispatch(addGroup(group));
+        props.startAddGroup(group);
         props.history.push('/');
       }}
     />
   </div>
 );
 
-export default connect()(AddGroupPage);
+const mapDispatchToProps = (dispatch) => ({
+  startAddGroup: (group) => dispatch(startAddGroup(group))
+});
+
+export default connect(undefined, mapDispatchToProps)(AddGroupPage);
