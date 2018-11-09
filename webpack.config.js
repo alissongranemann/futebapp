630a641c421cf7345329a77639023a5ffe09cfcf
Alisson Abreu <alisson@khomp.com>
2018-11-09 19:42:06 -0200

    using cache

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -12,47 +14,45 @@ if (process.env.NODE_ENV === 'test') {
 
 module.exports = (env) => {
     const isProduction = env === 'production';
-    const CSSExtract = new ExtractTextPlugin('styles.css');
 
     return {
-        entry: './src/main/js/app.js',
+        entry: { main: './src/main/js/app.js' },
         output: {
-            path: path.join(__dirname, 'public', 'dist'),
-            filename: 'bundle.js',
+            path: path.join(__dirname, 'dist'),
+            filename: '[name].[chunkhash:8].js'
         },
         module: {
-            rules: [{
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader'],
-            },
-	    {
-                test: /\.s?css$/,
-                use: CSSExtract.extract({
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader', 'eslint-loader'],
+                },
+                {
+                    test: /\.scss$/,
                     use: [
                         {
-                            loader: 'css-loader',
+                            loader: MiniCssExtractPlugin.loader,
                             options: {
                                 sourceMap: true,
-                            },
+                            }
                         },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true,
-                            },
-                        },
-                    ],
-                }),
-            }],
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+            ],
         },
         plugins: [
-            CSSExtract,
+            new CleanWebpackPlugin('dist', {} ),
+            new MiniCssExtractPlugin({
+                filename: 'styles.[contenthash:8].css',
+            }),
             new webpack.DefinePlugin({
                 'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
                 'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
@@ -61,14 +61,21 @@ module.exports = (env) => {
                 'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
                 'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
             }),
+            new HtmlWebpackPlugin({
+                inject: true,
+                hash: true,
+                template: './public/index.html',
+                filename: './index.html'
+                //TODO: index sumindo nas rotas
+            }),
         ],
         mode: isProduction ? 'production' : 'development',
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
-            contentBase: path.join(__dirname, 'public'),
+            contentBase: path.join(__dirname, 'dist'),
             compress: true,
             historyApiFallback: true,
-            publicPath: '/dist/',
+            publicPath: '.',
             port: 3000,
             watchContentBase: true,
             watchOptions: {
@@ -81,6 +88,25 @@ module.exports = (env) => {
                 path.resolve('./src/main/css'),
                 path.resolve('./node_modules'),
             ],
+        },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "vendor",
+                        chunks: "initial",
+                        enforce: true
+                    },
+                    // styles: {
+                    //     name: 'styles',
+                    //     test: /\.scss$/,
+                    //     chunks: 'all',
+                    //     enforce: true
+                    // }
+                },
+            },
+            runtimeChunk: true,
         },
     };
 };
