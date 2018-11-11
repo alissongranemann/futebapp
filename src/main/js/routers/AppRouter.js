6d3f350304277c34e8740b299f325d1135cfb4b7
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2018-11-11 20:59:55 -0200

    code split start

--- src/main/js/routers/AppRouter.js
+++ src/main/js/routers/AppRouter.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import GroupDashboardPage from 'components/GroupDashboardPage';
+import ErrorBoundary from 'components/common/ErrorBoundary';
+
 import AddGroupPage from 'components/group/AddGroupPage';
 import EditGroupPage from 'components/group/EditGroupPage';
 import ViewGroupPage from 'components/group/ViewGroupPage';
@@ -14,8 +15,11 @@ import LoginPage from 'components/login/LoginPage';
 import SignUpPage from 'components/login/SignUpPage';
 import ResetPasswordPage from 'components/login/ResetPasswordPage';
 import EditPlayerPage from 'components/player/EditPlayerPage';
+
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
+
+const GroupDashboardPage = lazy(() => import('components/GroupDashboardPage'));
 
 export const history = createBrowserHistory();
 
@@ -23,20 +27,24 @@ const AppRouter = () => (
     <Router history={history}>
         <React.Fragment>
             <CssBaseline />
-            <Switch>
-                <PublicRoute path="/" component={LoginPage} exact={true} />
-                <PublicRoute path="/signup" component={SignUpPage} />
-                <PublicRoute path="/reset" component={ResetPasswordPage} exact={true} />
-                <PrivateRoute path="/dashboard" component={GroupDashboardPage} />
-                <PrivateRoute path="/group/create" component={AddGroupPage} />
-                <PrivateRoute path="/group/edit/:id" component={EditGroupPage} />
-                <PrivateRoute path="/group/:id" component={ViewGroupPage} exact={true} />
-                <PrivateRoute path="/group/:groupId/match/create" component={AddMatchPage} />
-                <PrivateRoute path="/match/edit/:id" component={EditMatchPage} />
-                <PrivateRoute path="/group/:groupId/player/create" component={AddPlayerPage} />
-                <PrivateRoute path="/player/edit/:id" component={EditPlayerPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
+            <ErrorBoundary>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Switch>
+                        <PublicRoute path="/" component={LoginPage} exact={true} />
+                        <PublicRoute path="/signup" component={SignUpPage} />
+                        <PublicRoute path="/reset" component={ResetPasswordPage} exact={true} />
+                        <PrivateRoute path="/dashboard" component={GroupDashboardPage} />
+                        <PrivateRoute path="/group/create" component={AddGroupPage} />
+                        <PrivateRoute path="/group/edit/:id" component={EditGroupPage} />
+                        <PrivateRoute path="/group/:id" component={ViewGroupPage} exact={true} />
+                        <PrivateRoute path="/group/:groupId/match/create" component={AddMatchPage} />
+                        <PrivateRoute path="/match/edit/:id" component={EditMatchPage} />
+                        <PrivateRoute path="/group/:groupId/player/create" component={AddPlayerPage} />
+                        <PrivateRoute path="/player/edit/:id" component={EditPlayerPage} />
+                        <Route component={NotFoundPage} />
+                    </Switch>
+                </Suspense>
+            </ErrorBoundary>
         </React.Fragment>
     </Router>
 );
